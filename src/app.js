ceb5219108dadc4d16fa864e003da69d71cfa6cc
carloss-ziegler <chziegler445@gmail.com>
2023-01-04 08:11:45 -0300

    updates

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { router } from "./routes/router.js";
 import multer from "multer";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const app = express();
 
@@ -13,6 +16,12 @@ app.use((req, res, next) => {
 });
 app.use(cors());
 app.use(cookieParser());
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`conectado na port ${PORT}`);
+});
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {

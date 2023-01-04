aa35f85e407dbe750d646d73cfb6c047fc184172
carloss-ziegler <chziegler445@gmail.com>
2023-01-04 01:04:19 -0300

    Updates

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { router } from "./routes/router.js";
+import multer from "multer";
 
 export const app = express();
 
@@ -12,5 +13,22 @@ app.use((req, res, next) => {
 });
 app.use(cors());
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../../BookClub-Mobile/public/uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/uploads", upload.single("file"), (req, res) => {
+  const file = req.file;
+
+  req.statusCode(200).json(file.filename);
+});
 
 app.use(router);
